78979ad8f885e3a6b5b12f7635860c19a7250674
Joseph Murphy <jmurphy5277@gmail.com>
2014-02-13 15:59:39 -0500

    commit

--- chapter3/printProfile.py
+++ chapter3/printProfile.py
@@ -2,6 +2,43 @@
 
 import sqlite3
 
+#Connect to the Calls and Conversations tables to create a call log
+def printCallLog(skypeDB):
+	#connect to the sqlite3 db
+	conn = sqlite3.connect(skypeDB)
+	c = conn.cursor()
+	
+	#create a SQL query that pulls from both the Calls and Conversations tables joined by a conversation.id
+	c.execute("SELECT datetime(begin_timestamp, 'unixepoch,), identity FROM calls, conversations WHERE calls.conv_dbid = conversations.id;")
+	
+	#print the results for the call log
+	print '[*] -- Calls Found --'
+	for row in c:
+		print '[+] Time: ' + str(row[0]) + ' | Partner: ' + str(row[1])
+		
+
+#connect to the contacts table and print out contact information
+def printContacts(skypeDB):
+	#connect to the sqlite3 db
+	conn = sqlite3.connect(skypeDB)
+	c = conn.cursor()
+	
+	#pass the SQL query to the database to pull contact information
+	c.execute("SELECT displayname, skypename, city, country, phone_mobile, birthday FROM Contacts;")
+	
+	#print out the results of the query. Use if statements to ignore blank fields
+	for row in c:
+		print "[*] -- Found Contact --"
+		print '[+] User: ' + str(row[0])
+		print '[+] Skype Username: ' + str(row[1])
+		if str(row[2]) != '' and str(row[2]) != 'None':
+			print '[+] Location: ' + str(row[2]) + ', ' + str(row[3])
+		if str(row[4]) != 'None':
+			print '[+] Mobile Number: ' + str(row[4])
+		if str(row[5]) != 'None':
+			print '[+] Birthday: ' + str(row[5])
+
+#connect to the user accounts table, print out account information
 def printProfile(skypeDB):
 	#connect to the sqlite3 db
 	conn = sqlite3.connect(skypeDB)
